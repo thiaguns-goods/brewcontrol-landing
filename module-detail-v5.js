98ec8e4a97e6672835c104ef172db5721fce6629
thiaguns-goods <thiaguns@gmail.com>
2026-09-06 17:47:43 -0300

    chore: remove unused module v5 wrapper script

--- module-detail-v5.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  'use strict';
-  const script = document.createElement('script');
-  script.src = 'module-production-v5.js?v=20260906a';
-  script.defer = true;
-  document.head.appendChild(script);
-})();
