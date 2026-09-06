ff75c6ceece48d6b0751c355a63f0142b9af70d6
thiaguns-goods <thiaguns@gmail.com>
2026-09-06 17:31:00 -0300

    refactor: add shared v5 module script entrypoint

---
+++ module-detail-v5.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+  const script = document.createElement('script');
+  script.src = 'module-production-v5.js?v=20260906a';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
